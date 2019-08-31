35f7f6d407295dad854b6a7a9ce4cc8aab34e8f6
Kensuke Takahara <zawcjk@gmail.com>
2019-09-01 00:58:33 +0900

    practice depth first search.

--- src/book/chapter_2.cc
+++ src/book/chapter_2.cc
@@ -1,3 +1,5 @@
+#include <cstdio>
+
 int fact(int n) {
   if (n == 0) return 1;
   return n * fact(n - 1);
@@ -6,4 +8,35 @@ int fact(int n) {
 int fib(int n) {
   if (n <= 1) return n;
   return fib(n - 1) + fib(n - 2);
+}
+
+#define MAX_N 1000
+
+int memo[MAX_N + 1];
+
+int fib_memo(int n) {
+  if (n <= 1) return n;
+  if (memo[n] != 0) return memo[n];
+  return memo[n] = fib_memo(n - 1) + fib_memo(n - 2);
+}
+
+int a[MAX_N];
+int n, k;
+
+bool dfs(int i, int sum) {
+  // n個決定済み
+  if (i == n) return sum == k;
+  // a[i]を使用しない
+  if (dfs(i + 1, sum)) return true;
+  // a[i]を使用する
+  if (dfs(i + 1, sum + a[i])) return true;
+  // 上記のいずれのケースにも当てはまらずkが作れない
+  return false;
+}
+
+void solve() {
+  if (dfs(0, 0))
+    printf("Yes\n");
+  else
+    printf("No\n");
 }
