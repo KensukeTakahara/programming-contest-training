12c131994cab9d7b0d23ff65c6671725fa84dd78
Kensuke Takahara <zawcjk@gmail.com>
2019-08-31 18:28:46 +0900

    practice AtCoder ABC135 of C.

---
+++ src/AtCoder/ABC/135/C.cc
@@ -0,0 +1,53 @@
+#include <algorithm>
+#include <cstdio>
+
+using namespace std;
+
+int getIndex(int* arr, int index) {
+  if ((arr[index - 1] >= arr[index]) && (arr[index - 1] >= arr[index + 1])) {
+    return index - 1;
+  }
+  return arr[index] >= arr[index + 1] ? index : index + 1;
+}
+
+int main() {
+  int n;
+  scanf("%d", &n);
+
+  int city[n + 1];
+  long sum = 0;
+  int saver[n];
+
+  for (int i = 0; i < n + 1; ++i) {
+    int a;
+    scanf("%d", &a);
+    city[i] = a;
+    sum += a;
+  }
+  for (int i = 0; i < n; ++i) {
+    int a;
+    scanf("%d", &a);
+    saver[i] = a;
+  }
+
+  for (int i = 0; i < n; ++i) {
+    int current_saver = saver[i];
+    int current_city = city[i];
+    int next_city = city[i + 1];
+
+    if (current_saver >= current_city) {
+      city[i] = 0;
+      city[i + 1] = max(next_city - (current_saver - current_city), 0);
+    } else {
+      city[i] = current_city - current_saver;
+    }
+  }
+
+  long cnt = 0;
+  for (int i = 0; i < n + 1; ++i) {
+    cnt += city[i];
+  }
+  printf("%ld\n", sum - cnt);
+
+  return 0;
+}
