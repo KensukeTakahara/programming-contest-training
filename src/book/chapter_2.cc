bbf7a1a9ddbe48e9cacfea12d3aa9056f421e6da
Kensuke Takahara <zawcjk@gmail.com>
2019-09-01 20:39:07 +0900

    practice next permutation.

--- src/book/chapter_2.cc
+++ src/book/chapter_2.cc
@@ -84,3 +84,36 @@ int bfs() {
   }
   return d[gx][gy];
 }
+
+bool used[MAX_N];
+int perm[MAX_N];
+
+void permutation1(int pos, int n) {
+  if (pos == n) {
+    // permに対する操作
+    return;
+  }
+  for (int i = 0; i < n; ++i) {
+    if (!used[i]) {
+      perm[pos] = i;
+      used[i] = true;
+      permutation1(pos + 1, n);
+      used[i] = false;
+    }
+  }
+  return;
+}
+
+#include <algorithm>
+
+int perm2[MAX_N];
+
+void permutation2(int n) {
+  for (int i = 0; i < n; ++i) {
+    perm2[i] = i;
+  }
+  do {
+    // perm2に対する操作
+  } while (next_permutation(perm2, perm2 + n));
+  return;
+}
