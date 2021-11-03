9e88f42db5fc143c81afa0f89fb2583cd0c214e3
Kensuke Takahara <zawcjk@gmail.com>
2021-11-03 22:55:04 +0900

    performance tuning of 224C.

--- src/AtCoder/ABC/224/C.py
+++ src/AtCoder/ABC/224/C.py
@@ -1,20 +1,24 @@
-N = int(input())
-arr = [0, 0] * N
+def main():
+    N = int(input())
+    arr = [0, 0] * N
 
-for i in range(0, N):
-    arr[i] = [int(s) for s in input().split(' ')]
+    for i in range(0, N):
+        arr[i] = [int(s) for s in input().split(' ')]
 
-vector = list()
-for i in range(0, N - 1):
-    vector.append(list())
-    vector[i] = [[arr[j][0] - arr[i][0], arr[j][1] - arr[i][1]]
-                 for j in range(i + 1, N)]
+    vector = list()
+    for i in range(0, N - 1):
+        vector.append(list())
+        vector[i] = [[arr[j][0] - arr[i][0], arr[j][1] - arr[i][1]]
+                     for j in range(i + 1, N)]
 
-cnt = 0
-for i in range(0, len(vector)):
-    for j in range(0, len(vector[i])):
-        for k in range(j + 1, len(vector[i])):
-            if (vector[i][j][0] * vector[i][k][1]) - (vector[i][j][1] * vector[i][k][0]) != 0:
-                cnt += 1
+    cnt = 0
+    for vector_i in vector:
+        for j in range(0, len(vector_i) - 1):
+            for k in range(j + 1, len(vector_i)):
+                if (vector_i[j][0] * vector_i[k][1]) - (vector_i[j][1] * vector_i[k][0]) != 0:
+                    cnt += 1
 
-print(cnt)
+    print(cnt)
+
+
+main()
